5ce27ad7d42747822b6647b7f4fb6bb952b3d48e
MFredX <47466168+MFredX@users.noreply.github.com>
2020-10-15 22:17:41 +1000

    Preping for create samples

--- helper_scripts/create_bg_file.py
+++ helper_scripts/create_bg_file.py
@@ -1,15 +1,17 @@
 import cv2
-import numpy as np 
+import numpy as np
 import os
 import sys
 
+
 def create_bg_file():
-	for img in os.listdir(sys.argv[1]):
-		# create file path string
-		print(img)
-		line = 'neg_targetA_greyed_and_rescaled' + '/' + img + '\n'
-		# write line to file
-		with open('../bg.txt','a+') as f:
-			f.write(line)
+    for img in os.listdir(sys.argv[1]):
+        # create file path string
+        print(img)
+        line = 'neg_targetA' + '/' + img + '\n'
+        # write line to file
+        with open('../bg.txt', 'a+') as f:
+            f.write(line)
+
 
 create_bg_file()
